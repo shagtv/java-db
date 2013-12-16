f00cfe116e371eafea21ebfca7f2228c616bb62f
timur <timur@timur>
2013-12-17 00:01:22 +0400

    add  tests

--- test/ru/fizteh/fivt/students/zinnatullin/junit/DBTableTest.java
+++ test/ru/fizteh/fivt/students/zinnatullin/junit/DBTableTest.java
@@ -1,9 +1,7 @@
 package ru.fizteh.fivt.students.zinnatullin.junit;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
